3733564cb684b64d4a73c8a2494246af12efef7c
Devanshi Jagadale <devanshi.jagadale@gmail.com>
2026-06-09 15:32:05 +0530

    cors issue

--- backend/main.py
+++ backend/main.py
@@ -11,7 +11,7 @@ os.makedirs("static/images", exist_ok=True)
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://voice-text2sql.vercel.app/", "http://localhost:5173"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
